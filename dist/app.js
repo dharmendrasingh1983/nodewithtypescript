7e4b2cb392276ad719a70f81aaa65f62e697bcea
dharmendrasingh1983 <dharmendra1983@gmail.com>
2018-07-24 16:43:19 +0530

    pdfFolderCreating

--- dist/app.js
+++ dist/app.js
@@ -1,10 +1,13 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const express = require("express");
+const pdfCreater_1 = require("./pdfCreater");
+const humamusPdf_1 = require("./humamusPdf");
 class App {
     constructor() {
         this.express = express();
         this.mountRoutes();
+        this.pdfCreator = pdfCreater_1.PdfCreater;
     }
     mountRoutes() {
         const router = express.Router();
@@ -12,6 +15,17 @@ class App {
             res.json({
                 message: 'Hello World!'
             });
+        });
+        router.get('/pdf', (req, res, callback) => {
+            // let pf = new PdfCreater();
+            //pf.getPdf(res);     
+            let hummPdfCreater = new humamusPdf_1.HummmsPdf();
+            hummPdfCreater.getHummusPdf(res);
+            /* res.setHeader('Content-disposition', 'attachment; filename=theDocument.pdf');
+            res.setHeader('Content-type', 'text/plain');
+            res.charset = 'UTF-8';
+            res.write("Hello, world");
+            res.end(); */
         });
         this.express.use('/', router);
     }
